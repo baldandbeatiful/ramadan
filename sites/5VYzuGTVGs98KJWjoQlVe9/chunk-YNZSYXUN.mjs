0318fffa6405cea1fa39fa681ccb8f29959f3b49
baldandbeatiful <162518693+baldandbeatiful@users.noreply.github.com>
2024-03-31 00:29:28 +0300

    Add files via upload

---
+++ sites/5VYzuGTVGs98KJWjoQlVe9/chunk-YNZSYXUN.mjs
@@ -0,0 +1,2 @@
+import{o as a,p as o}from"./chunk-B32V74CY.mjs";var r="default"in o?a:o,t={},e=r;t.createRoot=e.createRoot;t.hydrateRoot=e.hydrateRoot;var s=t.createRoot,f=t.hydrateRoot;export{s as a,f as b};
+//# sourceMappingURL=chunk-YNZSYXUN.mjs.map
